b30a75d0e3165f3d5dd838e9733bdbdb055c459f
Barbara Tess <barbaratessn@gmail.com>
2024-06-18 17:01:53 +0300

    Addition of homepage styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/homepage/Home'
+
 function App() {
     return (
         <>
-            <h1>Task Management System Homepage.</h1>
+            <Router>
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Home />}
+                    />
+                </Routes>
+            </Router>
         </>
     )
 }
