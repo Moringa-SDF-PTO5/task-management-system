12a9b98c7a49cb4ddc76133f3f5bf464ad79dc92
Sylviachelimo <chelimosylvia280@gmail.com>
2024-06-20 10:13:52 +0300

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/homepage/Home';
 import Signup from './pages/signup/Signup';
+import Login from './pages/login/Login';
 import Dashboard from './components/Dashboard';
 import UserDetails from './components/UserDetails';
 import AddTask from './components/AddTask';
 
 function App() {
+    const [user, setUser] = useState({});
+
     return (
         <Router>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/signup' element={<Signup />} />
+                <Route path='/login' element={<Login setUser={setUser} />} />
                 <Route path='/dashboard' element={<Dashboard />} />
                 <Route path='/user/:userId' element={<UserDetails />} />
                 <Route path='/add-task' element={<AddTask />} />
