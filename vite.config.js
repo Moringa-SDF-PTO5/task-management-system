9b60baec0a00944fdc2dc0ae277d7707be221824
Barbara Tess <92345738+b-tess@users.noreply.github.com>
2024-06-23 16:05:52 +0300

    Update vite.config.js
    Production build config changes tryout.

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [react()],
-    server: {
-        port: 3000,
-        cors: true,
-        proxy: {
-            '/api': {
-                target: 'https://task-management-system-backend-39v0.onrender.com',
-                changeOrigin: true,
-                secure: false,
-                // rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        },
+    build: {
+        assetsDir: 'static',
     },
+    // server: {
+    //     port: 3000,
+    //     cors: true,
+    //     proxy: {
+    //         '/api': {
+    //             target: 'https://task-management-system-backend-39v0.onrender.com',
+    //             changeOrigin: true,
+    //             secure: false,
+    //             // rewrite: (path) => path.replace(/^\/api/, ''),
+    //         },
+    //     },
+    // },
 })
