0de4c2be98de280016c5658c6291cdfc3bfe7fda
Barbara Tess <92345738+b-tess@users.noreply.github.com>
2024-06-23 18:30:02 +0300

    Update vite.config.js
    Conditional check fail for the proxy. Vite does NOT support proxying in production.

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// const prodEnv = import.meta.env.PROD
-// const devEnv = import.meta.env.DEV
-const defaultConfig = {
-    plugins: [react()],
-}
-
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-    if (import.meta.env.PROD) {
-        return {
-            ...defaultConfig,
-            server: {
-                // port: 3000,
-                cors: true,
-                proxy: {
-                    '/api': {
-                        target: 'https://task-management-system-backend-39v0.onrender.com',
-                        changeOrigin: true,
-                        secure: false,
-                        // rewrite: (path) => path.replace(/^\/api/, ''),
-                    },
-                },
-            },
-        }
-    } else {
-        return {
-            ...defaultConfig,
-            server: {
-                port: 3000,
-                cors: true,
-                proxy: {
-                    '/api': {
-                        target: 'http://localhost:5555',
-                        changeOrigin: true,
-                        secure: false,
-                        // rewrite: (path) => path.replace(/^\/api/, ''),
-                    },
-                },
-            },
-        }
-    }
-    
+export default defineConfig({
+    plugins: [react()],
+    // server: {
+    //     port: 3000,
+    //     cors: true,
+    //     proxy: {
+    //         '/api': {
+    //             target: 'https://task-management-system-backend-39v0.onrender.com',
+    //             changeOrigin: true,
+    //             secure: false,
+    //             // rewrite: (path) => path.replace(/^\/api/, ''),
+    //         },
+    //     },
+    // },
 })
