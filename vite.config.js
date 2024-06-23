b033c1fc7d0aaf819e3e0cbdcae6d1825cb32da0
Barbara Tess <92345738+b-tess@users.noreply.github.com>
2024-06-23 17:53:04 +0300

    Update vite.config.js
    Conditional checking of the mode to decide which proxy server to run.

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: './',
+// const prodEnv = import.meta.env.PROD
+// const devEnv = import.meta.env.DEV
+const defaultConfig = {
     plugins: [react()],
-    build: {
-        assetsDir: 'static',
-    },
-    // server: {
-    //     port: 3000,
-    //     cors: true,
-    //     proxy: {
-    //         '/api': {
-    //             target: 'https://task-management-system-backend-39v0.onrender.com',
-    //             changeOrigin: true,
-    //             secure: false,
-    //             // rewrite: (path) => path.replace(/^\/api/, ''),
-    //         },
-    //     },
-    // },
+}
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+    if (import.meta.env.PROD) {
+        return {
+            ...defaultConfig,
+            server: {
+                // port: 3000,
+                cors: true,
+                proxy: {
+                    '/api': {
+                        target: 'https://task-management-system-backend-39v0.onrender.com',
+                        changeOrigin: true,
+                        secure: false,
+                        // rewrite: (path) => path.replace(/^\/api/, ''),
+                    },
+                },
+            },
+        }
+    } else {
+        return {
+            ...defaultConfig,
+            server: {
+                port: 3000,
+                cors: true,
+                proxy: {
+                    '/api': {
+                        target: 'http://localhost:5555',
+                        changeOrigin: true,
+                        secure: false,
+                        // rewrite: (path) => path.replace(/^\/api/, ''),
+                    },
+                },
+            },
+        }
+    }
+    
 })
